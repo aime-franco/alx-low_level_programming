30d897d5606b562165c9f398aad21b82e0c3ed83
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-10 03:14:08 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+
+int main(void)
+{
+	printf("with proper grammer, the outcomes are piece of art,\n");
+	return (0);
+}
