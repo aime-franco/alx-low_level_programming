1d474eb88014286e7c4be66209b3f39b71d11873
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-11 01:01:12 -0700

    putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after excuting the code
+ */
+int main(void)
+{
+	char low;
+
+	for (low = "a", low <= "z", low++)
+		putchar(low);
+	return (0);
+}
