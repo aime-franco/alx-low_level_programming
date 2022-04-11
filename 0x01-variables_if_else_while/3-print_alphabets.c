54bb98388c53b94d0f7df042e120bcc762da81fb
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-11 05:49:30 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	char low;
 	
-	for (low = 'a'; low <= 'z'"\n"; low++)
+	for (low = 'a'; low <= "z\n"; low++)
 		putchar(low);
-	for (low = 'A'; low <= 'Z'"\n"; low++)
+	for (low = 'A'; low <= "Z\n"; low++)
 		putchar(low);
 	return (0);
 }
