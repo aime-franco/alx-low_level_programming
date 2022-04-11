46c968d3732b0426de8960e880247d39a75cea39
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-11 08:09:16 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after excuting code
+ */
 int main(void)
 {
 	char low, e, q;
+	
 	e = 'e';
 	q = 'q';
 
