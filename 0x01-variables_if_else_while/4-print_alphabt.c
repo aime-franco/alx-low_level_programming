bfa1aa9d2a7c45023fa5d1faf8eda1e43e806470
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-11 07:48:41 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+int main(void)
+{
+	char low;
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low = 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	puchar('\n');
+	return (0);
+}
