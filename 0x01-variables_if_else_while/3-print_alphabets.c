ba2c9a57c58e1cdeb63c155b39bf90472fc9d384
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-11 05:06:56 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: 0 after excuting code
+ */
+int main(void)
+{
+	char low;
+	
+	for (low = 'a'; low <= 'z'; low++);
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++);
+		putchar(low);
+	return (0);
+}
+
