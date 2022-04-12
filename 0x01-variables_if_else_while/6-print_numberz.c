466f7d6e8144d3f1ce772083f16753c029d14e2a
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-11 17:02:32 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after excuting codde
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums < 10; nums++)
+		putchar(nums);
+	putchar('\n');
+	return (0);
+}
