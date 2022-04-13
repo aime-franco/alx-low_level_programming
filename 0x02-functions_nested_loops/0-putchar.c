cd0dc9d70318a283d8079f2d2aa1c322186f2ed1
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-13 02:56:40 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/** 
+ * main - Entry point
+ *
+ * Return: 0 after code excution
+ */
+int main(void)
+{
+	write("_putchar\n");
+	return (0);
+}
