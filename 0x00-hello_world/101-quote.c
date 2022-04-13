42cd657e836a581d16fa50983565fba5737733cc
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-12 19:54:57 -0700

    write() function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 after finding error in code
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+}
