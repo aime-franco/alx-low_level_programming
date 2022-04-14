a606f0c81ffd77c069df722a59edecb5eb2d9c59
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-14 06:27:16 -0700

    fixing the syntax errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,17 @@
 void print_alphabet_x10(void)
 {
 	int count;
-	char alpha;
+	int alpha;
 
 	count = 0;
 	while (count < 0)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{	
 			_putchar(alpha);
 	}
 	count ++;
 	_putchar('\n');
+	}
 }
+
