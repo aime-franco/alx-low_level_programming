4decd826eb12942bb4ea4090de274865b519ae86
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-14 05:12:11 -0700

    while loop

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet
+ *
+ * Return: 0 after code excution
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	char alpha;
+
+	count = 0;
+	while (count < 0)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	}
+	count ++
+	_putchar('\n')
+}
