eeb9257ee90a6e69f96e8320f6d863d3763b550b
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-14 15:54:23 -0700

    isalpha function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks the alphabet character
+ * @c: contain comapre value
+ * Return: 0 success after code excution
+ */
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'a' && c < 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
