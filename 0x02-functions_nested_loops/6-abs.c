8112c52d384321c41544dec64718ea743719a721
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-14 17:16:20 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value 
+ * @n: contains the compared value
+ * Return: 0 success after excuting the code
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n * (-1));
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
