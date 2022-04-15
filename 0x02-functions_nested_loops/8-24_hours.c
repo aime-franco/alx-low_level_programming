8f600c8df066c79b7b8f2b80d7b0f385ab12d84b
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-14 18:22:49 -0700

    jack bauer function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * jack_bauer - prints every minutes of the day
+ *
+ * Return: 0 after code excution
+ */
 void jack_bauer(void)
 {
 	int s;
@@ -6,13 +11,14 @@ void jack_bauer(void)
 
 	for (s = '0'; s <= '23'; s++)
 	{
-		for (m = '0'; m <= '59'; m++)
-			_putchar(s / 10 + '0');
-			_putchar(s % 10 + '0');
-			_putchar(':');
-			_putchar(m / 10 + '0');
-			_putchar(m % 10 + '0');
-			_putchar('\n');
-
+	for (m = '0'; m <= '59'; m++)
+	{
+		_putchar(s / 10 + '0');
+		_putchar(s % 10 + '0');
+		_putchar(':');
+		_putchar(m / 10 + '0');
+		_putchar(m % 10 + '0');
+		_putchar('\n');
+	}
 	}
 }
