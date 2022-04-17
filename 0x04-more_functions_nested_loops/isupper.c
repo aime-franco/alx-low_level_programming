b8c2001cee9e97ced75ca496c7fa00c012361b16
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-17 08:42:04 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper -check the upper case alphabet
+ *
+ * Return: 0 success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A'; c <= 'Z'; c++); 
+}
