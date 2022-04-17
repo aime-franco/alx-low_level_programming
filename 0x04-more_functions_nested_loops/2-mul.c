94dccde906c8de614a70f4975db6eee4e8304a12
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-17 19:16:17 +0200

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - multiply the values
+* @a: contains the value for a
+* @b: contains the value for b
+* Return: 0 success
+*/
+int mul(int a, int b)
+{
+  return (a * b);
+}
+  
+  
