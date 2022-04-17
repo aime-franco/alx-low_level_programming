fac33a6ff3206ee115237a354d14927384856afe
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-17 09:40:15 -0700

    fixing the errors

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * _isupper -check the upper case alphabet
- *
+ * @c: contains compared values 
  * Return: 0 success
  */
 int _isupper(int c)
 {
-	return (c >= 'A'; c <= 'Z'; c++);
+	return (c >= 'A' && c <= 'Z');
 }
