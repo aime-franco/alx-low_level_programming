d5ed432c816c900a704ee785cdf369370bda1375
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-18 13:01:02 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * print_numbers - print the number from 0 to 9
@@ -17,6 +16,3 @@ void print_numbers(void)
 	}
 	_putchar('\n');
 }
-=======
-
->>>>>>> 904bc7a3c9ebc5a6ed71957563100b76e8ddfa1c
