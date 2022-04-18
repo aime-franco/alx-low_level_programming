5259a3ff8124f689996cd4c8134b68a779739d5e
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-19 01:14:10 +0200

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers from 0 to 9
+* 
+* Return: 0 success
+*/
+void print_numbers(void)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}	
