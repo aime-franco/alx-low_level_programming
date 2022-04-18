45b2929569fd41c996d615a3f768080342dc66f2
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-18 16:03:16 -0700

    changes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - print digit from 0 to 9
+ * @c: contains compared value
+ * Return: 0 success
+ */
+int _isdigit(int c)
+{
+	if (c = 0; c <= 9; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
