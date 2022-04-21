021ac1046bd0f9d5c0e7bb818bfd15e79487eca8
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-20 23:45:53 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - prints the number from 0 to 9
+ * @c: contains the compare value
+ * Return: 0 success
+ */
+int _isdigit(int c)
+{
+	return (c = 0 && c <= 9 );
+}
