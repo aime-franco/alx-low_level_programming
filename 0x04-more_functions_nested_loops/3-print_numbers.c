5652ff8e3d1640e8dcfe291aa03a6efaa0a4229b
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-22 04:08:17 -0700

    adding the third task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: 0 success
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
