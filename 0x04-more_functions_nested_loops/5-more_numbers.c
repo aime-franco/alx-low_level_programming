3baf845bcdfb9c7158919f6a2c9b34f5baf666ae
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-22 12:05:42 -0700

    more numbers file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@
  */
 void more_numbers(void)
 {
-	int a, b;
+	char a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = '0'; a < '10'; a++)
 	{
-		for (b =0; b <= 14; b++)
+		for (b = '0'; b <= '14'; b++)
 		{
-			if (b > 9)
+			if (b > '9')
 			{
-				_putchar(y /10 +'0');
+				_putchar(y / 10 + '0');
 			}
 			_putchar(y % 10 + '0');
 		}
