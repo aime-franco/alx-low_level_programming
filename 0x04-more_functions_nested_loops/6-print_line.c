1add190322fec6c1636066609d5d80e58cb40e99
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-22 22:53:04 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,7 @@ void print_line(int n)
     {
         _putchar('_\n');
     }
-    else if (n < 0)
+    else if (n <= 0)
     {
         _putchar('\n');
-    }
-    else
-    {
-       _putchar('\n');
-    }    
 }
