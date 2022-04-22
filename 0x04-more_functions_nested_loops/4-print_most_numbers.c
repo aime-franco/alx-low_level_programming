2576f55e01f0664a6f65215644f225c58842007c
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-22 06:33:25 -0700

    updating most numbers file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints number expect 2 and 4
- *
- * Reutrn: 0 successs
+ * prin_most_numbers - print numbers except 2 and 4
  */
 void print_most_numbers(void)
 {
 	char c;
-	
-	for (c = '0'; c <= '9'; c++)
+
+	for (c = '0'; c <= '9'; c+)
 		if (c != '2' && c != '4')
 			_putchar(c);
 	_putchar('\n');
