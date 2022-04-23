9f73dae3895952a0839906358eaa1eea08ee2b5d
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-23 01:47:06 -0700

    creating print line file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,18 @@
 */
 void print_line(int n)
 {
-    if (n > 0)
-    {
-        _putchar('_');
-        _putchar('\n')
-    }
-    else if (n <= 0)
-    {
-        _putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
