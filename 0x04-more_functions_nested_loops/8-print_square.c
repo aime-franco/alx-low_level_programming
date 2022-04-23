dce1bf4d9e05d83270fcb8970bd4e0bc8a49956f
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-23 04:37:40 -0700

    update print square file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,21 +7,21 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else 
-	{
-		int a, b;
+	int a, b;
 
+	if (size > 0)
+	{
 		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
