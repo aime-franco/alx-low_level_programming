f53b69025bbcf278faabf97c84b2a0aec0e90652
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-23 01:32:30 -0700

    update more numbers file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* more_numbers - print numbers from 0 to 14
-*
-* Return: 0 success
-*/
+ * more_numbers - print the numbers from 0 to 14
+ *
+ * Return: 0 success
+ */
 void more_numbers(void)
 {
 	int a;
 	char b;
-	
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
