523b00d2bc5874613793588f43138175c14710d1
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-23 03:43:39 -0700

    create print diagonal file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal shape
+ * @n: contains compared value
+ * Return:0 success
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				if (b == a)
+					_putchar('\\');
+				else if (b < a)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
