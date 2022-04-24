c29ecc8cb567c48e87fcc9803aae34d861edef6b
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-24 02:23:51 -0700

    update fizzbuzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 15 == 0)
 		{
-			printf(" Fizz");
+			printf("FizzBuzz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 		}
-		else if (i % 5 == 0 && i % 3 == 0)
+		else if (i % 3 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("Fizz ");
 		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
-		else 
+		else
 		{
 			printf("%d", i);
 		}
