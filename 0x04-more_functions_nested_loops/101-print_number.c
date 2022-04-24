f0d1d7fb14060dbb5a002250e8a43e9aa4b42c02
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-24 16:25:42 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,19 +5,17 @@
  * @n: tracked var
  */
 
-void printnumber(int n)
+void print_number(int n)
 {
-
-    if (n < 0) {
+    if (n < 0) 
+    {
         _putchar('-');
         n = -n;
     }
-
     if (n == 0)
         _putchar('0');
 
     if (n / 10)
         print_number(n / 10);
-
     _putchar(n % 10 + '0');
 }
