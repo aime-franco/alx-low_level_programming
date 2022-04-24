466a128e51a22ef010a39f19cf1939b8fe2fbc90
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-24 17:00:35 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 
 				prod = num * mult;
 
-				if (prod <= 9)
+				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (prod <= 9 && prod >= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
 				}
