07a9745dd55c16704c164c6800ddebc5744a2bf8
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-24 14:25:30 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,16 @@
 
 void print_number(int n)
 {
-	unsigned int x;
-	
+	unsigned int x = n;
+
 	if (n < 0)
 	{
-		_putchar('-')
+		_putchar(45);
 		x = -n;
 	}
-	else
-	{
-		x = n;
-	}
-	if (x / 100 != 0)
+	if ((x / 10) > 0)
 	{
 		print_number(x / 10);
-		_putchar(x % 10 + '0');
 	}
-
+	_putchar((x % 10) + 48);
+}
