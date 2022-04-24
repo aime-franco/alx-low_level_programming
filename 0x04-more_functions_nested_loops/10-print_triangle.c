eb6dd37f91b2a2587c170cca0bb216a8cc68308f
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-24 14:04:12 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * print_triangle - print the triangle shape
- * @size: contains compared value
- * Return: 0 success
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
 void print_triangle(int size)
 {
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
 		int a, b;
-		
+
 		for (a = 1; a <= size; a++)
 		{
 			for (b = a; b < size; b++)
 			{
 				_putchar(' ');
 			}
+
 			for (b = 1; b <= a; b++)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
