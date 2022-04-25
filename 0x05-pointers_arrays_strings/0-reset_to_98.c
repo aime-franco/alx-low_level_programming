ad399fb167777bae7ac83493a87d47db71a20dae
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-24 17:49:19 -0700

    create 0-resset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - update the value by pointer
+ * @*n: contains compared value
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
