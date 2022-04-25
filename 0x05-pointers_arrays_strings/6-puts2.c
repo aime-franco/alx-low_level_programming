b641e774a60f061e8937a74277536b3806ce3554
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-25 05:36:49 -0700

    create 6-put2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - print the first character of string
+ * @str: contains the compared value
+ */
+void puts2(char *str)
+{
+	int x;
+	
+	x = 0;
+	while (str[x] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+		x++;
+	}
+	_putchar('\n');
+}
