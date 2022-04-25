4e47e9534f345daf0dc9c0592870e129b8b8a2c3
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-25 04:25:55 -0700

    create 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * print_rev - print the reversed string
+ * @s: contains the compared value
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int x;
+	int length = 0;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	for (x = length -1; x >= 0; x-- )
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+	}
+}
