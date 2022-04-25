768d21b96f13f450ad6dc6d767dae62853fe06dd
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-25 14:12:55 +0200

    create 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev-string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rev_string - reverses the string
+* @s: contains the compared value
+*/
+void rev_string(char *s)
+{
+  int length, j, i;
+	char v1, v2;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	j = length - 1;
+	i = 0;
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
+	}
+}
