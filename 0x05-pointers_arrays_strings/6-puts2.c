974f829a5838bcb9f88de5de5d94520ff6122e6a
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-25 14:42:45 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,13 @@
  */
 void puts2(char *str)
 {
-	int x;
+	int x = 0;
 	
-	x = 0;
-	while (str[x] != '\0')
+	while (str[x] != 0)
 	{
 		if (x % 2 == 0)
 		{
-			_putchar(str[x]);
+			_puchar(str[x]);
 		}
 		x++;
 	}
