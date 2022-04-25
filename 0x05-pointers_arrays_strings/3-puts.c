1645eaea854129840c1b659a8d69676584cd283c
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-24 18:48:20 -0700

    create 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints the srting as an output
+ * @str: contains the string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		puts(s[x]);
+	}
+	puts('\n');
+}
