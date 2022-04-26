d5dbc3504b43d06559692ea04ff14aa898b3c6b6
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-25 18:20:20 -0700

    update 7-puts+half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,15 @@
 */
 void puts_half(char *str)
 {
-        int length, n, x;
-  
-        for (length = 0; str[length] != '\0'; length++)
-        {
-        }
-        n = (length - 1) / 2;
-        for (x = n + 1; str[x] != '\0'; x++)
-        {
-               _putchar(str[x]);
-        }
-        _putchar('\n');
+	int length, n, x;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+	n = (length - 1) / 2;
+       for (x = n + 1; str[x] != '\0'; x++)
+       {
+      	 	_putchar(str[x]);
+       }
+       _putchar('\n');
 }
