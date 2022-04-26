1613d396aaf16f6635ee4ba053bd00d502a1a888
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-25 17:28:11 -0700

    update 5-puts3.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 
 /**
- * puts2 - print the first character of string
- * @str: contains the compared value
+ * puts2 - prints the  first character of a string
+ * @str: contains compared value
  */
 void puts2(char *str)
 {
 	int x = 0;
-	
-	while (str[x] != 0)
+
+	while(str[x] != '\0')
 	{
 		if (x % 2 == 0)
 		{
 			_putchar(str[x]);
 		}
-		x++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
