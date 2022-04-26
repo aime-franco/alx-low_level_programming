51cc228812b4a8f92738122abd7f23e1d8e1ea9c
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-04-26 03:06:40 +0200

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts_half - prints the half of string characters
+* @str: contains compared value
+*/
+void puts_half(char *str)
+{
+  int length, n, x;
+  
+  for (length = 0, str[length] != '\0'; length++)
+  {
+  }
+  n = (length - 1) / 2;
+  for (x = n + 1; str[x] != '\0'; x++)
+  {
+    _putchar(str[x]);
+  }
+  _putchar('\n');
+}
