4a648246a08fda8133134490074f01b8ec847629
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-25 18:36:19 -0700

    create 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prins the element of the array
+ * @a: contains compared value of a
+ * @n: contains compared value of n
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++ )
+	{
+		printf("%d", i);
+		if (x < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	prinf("\n");
+}
