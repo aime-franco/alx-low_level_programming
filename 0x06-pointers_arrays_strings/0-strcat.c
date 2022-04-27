3f622d7358528f10e0b8537d6d965d4e5e648c82
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-27 00:45:35 -0700

    create 0-strcat.c file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenate the strings
+ * @dest: contains compared value of dest
+ * @src: source of string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0, y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != 0)
+	{
+		src[y] = dest[x];
+		x++;
+		y++;
+	}
+	dest[x];
+	return (dest);
+}
