a16ee7d04bbf8fb41001e799b8c6b224142f6cd0
aime-franco <usanaseaimefrancois@gmail.com>
2022-04-29 12:37:41 -0700

    create 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies src into dest
+ * @dest: contains dest string
+ * @src: contains src string
+ * @n: number of byte from source
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0, y = 0;
+
+	while (n > y)
+	{
+		if (src[y] == '\0')
+		{
+			for (; y < n; y++)
+			{
+				dest[x] != '\0';
+				x++;
+			}
+		}
+		else
+		{
+			dest[x] = src[y];;
+			x++;
+			y++;
+		}
+	}
+	return dest;
+}
