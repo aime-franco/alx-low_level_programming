ff39fc5d410c1221e75d72ebddf17f98251d3f14
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-01 12:05:11 -0700

    update 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,39 @@
 #include "main.h"
 
 /**
- *cap_string - capitalizes all words of a string.
- *@s: string to use.
- *
- *Return: string.
+ * cap_string - capitalize the string
+ * @s: contains string
+ * Return: string
  */
-char *cap_string(char *s)
+char *cap_string(char *)
 {
 	int x = 1, y, check;
-	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+	char a[] = {',', '.', ';', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
 
-	if (s[0] > 96 && s[0] < 123)
+	if (s[0] >96 && s[0] < 123)
 	{
 		s[0] -= 32;
 	}
 	while (s[x] != '\0')
 	{
-		if (s[x] > 96 && s[x] < 123)
-		{
-			y = 0;
-			check = 0;
-			while (check == 0 && y < 13)
+			if (s[x] > 96 && s[x] < 123)
 			{
-				if (s[x - 1] == a[y])
+				y = 0;
+				check = 0;
+				while (check == 0 && y < 13)
 				{
-					check = 1;
+					if (s[x - 1] == a[y])
+					{
+						check = 1;
+					}
+					y++;
 				}
-				y++;
+				if (check == 1)
+				{
+					s[x] -= 32;
+				}
 			}
-			if (check == 1)
-			{
-				s[x] -= 32;
-			}
-		}
-		x++;
+			x++;
 	}
 	return (s);
 }
