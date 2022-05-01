22220e213275dfbf51697593c2166dab991ecee3
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-05-01 17:39:04 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  * @s: contains the string
  * Return: string
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i = 1, j, check;
 	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
