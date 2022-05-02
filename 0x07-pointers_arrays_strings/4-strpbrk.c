3a7736f7294a09dcf6a5739a59bc0407e2cc8b66
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-05-02 13:33:25 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,5 +8,11 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+  unsigned int i, j;
 
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				goto exit;
+exit: return (s[i] != '\0' ? s + i : '\0');
 }
