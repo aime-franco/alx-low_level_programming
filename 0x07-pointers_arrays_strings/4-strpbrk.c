c03216c463842974b27f9c8f632e43c1914fdb10
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-02 04:31:45 -0700

    create 4-strpbrk.c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search string
+ * @s: contains string
+ * @accept: contains compared value
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+}
