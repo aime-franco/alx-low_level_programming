aa6d5639698b7bac89ba744fefaf295dde1be3ec
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-02 04:13:36 -0700

    create 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a character in the string
+ * @s: contains string
+ * @c: contains character
+ * Return: string
+ */
+ char *_strchr(char *s, char c)
+{
+	int x = 0, y;
+
+	while (s[x])
+	{
+		X++;
+	}
+	for (y = 0, y <= x, y++)
+	{
+		if (x == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+	return ('\0');
+}
