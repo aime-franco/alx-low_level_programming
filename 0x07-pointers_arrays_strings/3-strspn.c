a894577766cd09c2bd52b164d5a681ed153a703d
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-02 04:24:56 -0700

    create 3-strspn.c file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of substring
+ * @s: contains the string
+ * @accept: contans compared value
+ * Return: string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x
+}
