95aa7067f73b4463a85cd5a8be5887d609548c6f
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-04 08:23:39 -0700

    create 4-pow_recursion.c file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @y: value of y
+ * @n: value of x
+ * return: 0 success
+ */
+int _pow_recursion(int x, int y)
+{
+	
+}
