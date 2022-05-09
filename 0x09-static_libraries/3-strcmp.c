6235d3857f7bd8b4031a48b7128d9ac86d74b0ad
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-05-09 11:41:44 +0200

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: contains string of s1
+ * @s2: contains string of s2
+ * Return: 0 success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+	}
+	return (0);
+}
