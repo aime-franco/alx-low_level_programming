53bec3896a5022b9ec5d486e9e9de021ed1b3be2
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-05-09 11:33:09 +0200

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenate the strings
+ * @dest: contains compared value of dest
+ * @src: source of string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0, y = 0;
+
+	while (dest[x] != '\0')
+		x++;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
