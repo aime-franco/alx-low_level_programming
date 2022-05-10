eeeb3cc81dffa5acb18fe91a082c380d4b49cbb0
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-09 18:15:37 -0700

    create 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ * @argc: contains the number of program
+ * @argv: contains the array of argc
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
