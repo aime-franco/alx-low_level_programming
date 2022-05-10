eaa660b7f39721871f5f87f4c0d8cc1709f75612
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-09 18:42:00 -0700

    create 2-arg.s.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: contains the number of parameters
+ * @argv: contains arrays of argc
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc > 0)
+	{
+		for (x = 0, x < argc, x++)
+		{
+			printf("%s\n", argv[x]);
+		}
+	}
+	return (0);
+}
