6d991f3e194accad63ba4695a2b7a714f3fb04f4
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-10 07:42:26 -0700

    create create_array file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * create_array - create the array
+ * @size: the size of memory
+ * @c: contains specific character
+ * Return: nothing
+ */
+char *create_array(unsigned int size, char c)
+{
+
+}
