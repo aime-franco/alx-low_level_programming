119311bd3157afc30f4a4109a98701b11c872c66
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-09 18:25:28 -0700

    create 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: contains the number of parameter
+ * @argv: contans the arrays of argc
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
