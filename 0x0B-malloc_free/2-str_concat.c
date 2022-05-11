9985ae5d7ba36fbad8a0e034db94222c538c9aec
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-10 19:57:16 -0700

    creat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concantinate the 2 strings
+ * @s1: string one
+ * @s2: string two
+ * Retrun: string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	
+}
