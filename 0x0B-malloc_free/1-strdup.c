29781dcfc7a27f11823ba76da3a63d0e6f8d8b28
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-10 19:37:48 -0700

    create 1-strdup,c file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns the pointer to its newly locatio
+ * @str: containd string
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	int x;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; str[x]; x++)
+	{
+	;
+	x++;
+	}
+	str = malloc(sizeof(char) * i);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		s[x] = str[x];
+	}
+	s[x] != '\0';
+	return (s);
+}
