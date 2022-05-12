f33aba75578b268280992066426a30760152711f
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-11 17:42:12 -0700

    creae 2-print_dog.c file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,21 @@ void print_dog(struct dog *d)
 	if (d != 0)
 	{
 		if ((*d).name != 0)
+		{
 			printf("Name: %s\n", (*d).name);
+		}
 		else
-			printf("Name:(nil)\n");
+		{
+			printf("Name: (nil)\n");
+		}
 		printf("Age: %f\n", (*d).age);
 		if ((*d).owner != 0)
+		{
 			printf("Name: %s\n", (*d).owner);
+		}
 		else
-			printf("Name: (nil)\n")
+		{
+			printf("Name: (nil)\n");
+		}
 	}
 }
