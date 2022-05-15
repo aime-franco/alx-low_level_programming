c91e3742f0dd622f0519aed076e7bcdb89de56be
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-15 13:18:24 +0200

    create 3-get_op_func.c file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ *
+ * get_op_func - select correct function
+ * @s: operator
+ * Return: 0 success
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = 
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod}
+	};
+	int x;
+
+	x = 0;
+	while (x < 5)
+	{
+		if (ops[x].op[0] == s[0])
+			return (ops[x].f);
+		x++;
+	}
+	return (NULL);
+}
