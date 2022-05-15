75c747a2c760584e864a80ebc8eb75a234199065
aime-franco <101458219+aime-franco@users.noreply.github.com>
2022-05-15 14:22:23 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,53 +2,66 @@
 #include <stdlib.h>
 
 /**
- *
- * op_add - add nuumbers
- * @a: first number
- * @b: second number
- * Return: sum of a and b
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - substract numbers
- * @a: first number
- * @b: second number
- * Return: difference of a and b
+ * op_sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - multiply numbers
- * @a: first number
- * @b: second number
- * Return: multiplication of a and b
+ * op_mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - divides numbers
- * @a: first number
- * @b: second number
- * Return: division of a and b
+ * op_div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
  */
-int op_div(int a. int b)
+int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 /**
- * op_mod - modulas of numbers
- * @a: first number
- * @b: second number
- * Return: remainder of a and b
+ * op_mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
  */
-int op-mod(int a, int b)
+int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
