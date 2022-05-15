eafc02f1bc9838342763a2894744a2a291f69891
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-15 12:46:46 +0200

    update 2-int_index.c file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,8 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int x;
 
 	if (size < 1 || array == NULL || cmp == NULL)
+	{
 		return (-1);
-	if (x = 0; x < size, x++)
+	}
+	if (x = 0; x < size; x++)
 	{
 		if (cmp(array[x]))
 			return (x);
