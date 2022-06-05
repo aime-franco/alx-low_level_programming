12ec3e5608e1a3680bf305434ddcdf1f24ce6d03
aime-franco <usanaseaimefrancois@gmail.com>
2022-06-05 18:48:49 +0200

    update add node file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,21 +7,17 @@ int len(const char *str);
  * Return: Pointer to the head of the linked list
  */
 list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node;
+{   list_t *new;
+    size_t nchar;
 
-	if (head == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	if (*head == NULL) /*Initialzes the next pointer to NULL*/
-		new_node->next = NULL;
-	else /*Moves the new node to the head*/
-		new_node->next = *head;
-	new_node->str = strdup(str);
-	new_node->len = len(str);
-	*head = new_node;
-	return (*head);
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+    new->str = strdup(str);
+    for (nchar = 0; str[nchar]; nchar++)
+        ;
+    new->len = nchar;
+    new->next = *head;
+    *head = new;
+    return (*head);
 }
