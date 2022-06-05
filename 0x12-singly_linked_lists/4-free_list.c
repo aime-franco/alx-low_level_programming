e6b1ec928aabaca66777632a8bad7fb28b9eea3d
aime-franco <usanaseaimefrancois@gmail.com>
2022-06-05 19:16:17 +0200

    create free list file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: contains the head of linked list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *old;
+
+	while ((old == head) != NULL)
+	{
+		head = head->next;
+		free(old->next);
+		free(old);
+	}
+}
