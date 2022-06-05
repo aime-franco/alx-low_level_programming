56de3e64e3536c3cdf5ebf14c55ca69b2686e3fd
aime-franco <usanaseaimefrancois@gmail.com>
2022-06-05 12:26:55 +0200

    update list len file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	int x;
+	size_t ways;
 
-	if (h == NULL)
-		return (0);
-	for (x = 1; h->next != NULL, x++)
+	ways = 0;
+	while (h != NULL)
 	{
 		h = h->next;
+		ways++;
 	}
-	return (x);
+	return (ways);
 }
