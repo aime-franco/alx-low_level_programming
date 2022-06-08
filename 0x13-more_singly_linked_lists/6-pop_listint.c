21fe77061b804de3b0219d28ec22d452f1795424
aime-franco <usanaseaimefrancois@gmail.com>
2022-06-08 02:22:42 +0200

    update pop listint file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,7 @@ int pop_listint(listint_t **head)
 	if (!head || !*head)
 		return (0);
 	num = (*head)->n;
-	num = (*head)->next;
+	old = (*head)->next;
 	free(*head);
 	*head = old;
 	return (num);
