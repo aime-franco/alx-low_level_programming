18942f07bf687bd78475d205f6d9726a068fbcea
aime-franco <usanaseaimefrancois@gmail.com>
2022-06-09 22:22:38 +0200

    update binary to int file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,23 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converters a binary number to an unsigned integer
- * @b: String representing a binary number
- * Return: The converted number or 0 if b is NULL or if there are chars
- * that are not 0 or 1
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
-	unsigned int i;
-	unsigned int j;
-	unsigned int sum;
-	unsigned int pow;
-	int base;
+    int i;
+    unsigned int dec_val = 0;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
-	if (b == NULL)
-		return (0);
-	for (len = 0; b[len] != '\0'; len++)
-		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		for (j = len - 1; j > 0; j--)
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48));
-		len--;
-		pow = 1;
-	}
-	return (sum);
+    if (!b)
+        return (0);
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
+    return (dec_val);
 }
