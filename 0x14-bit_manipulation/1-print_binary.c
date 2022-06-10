49ef46974d48cf4ec216cf5ff04b382fd13c7036
aime-franco <usanaseaimefrancois@gmail.com>
2022-06-10 10:23:06 +0200

    update print binary file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,30 @@
 #include "main.h"
+void print_b(unsigned long int n);
 
 /**
- * print_binary - print binary representation of a number
- * @n: contains the number
+ * print_binary - prints the binary representation of a number
+ * @h: contains the binary
  * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int current;
-	int x, count = 0;
-
-	for (x = 63; x >= 0; x--)
-	{
-		current = n >> x;
-		if (current & 1)
-		{
-			_putchar('1');
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
+	if (n == 0)
+		_putchar('0');
+	else
+		print_b(n);
+}
+/**
+ * print_b - prints the binary representation of a number
+ * @n: contains the binary
+ * Return: nothing
+ */
+void print_b(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_b((n >> 1));
+	if ((n & 1) == 1)
+		_putchar('1');
+	else if ((n & 1) == 0)
 		_putchar('0');
 }
